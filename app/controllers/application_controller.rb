2d8ea82246c3fb2961cc9b0906317a0d444eff1a
shelly <Yingli2@academic.rrc.ca>
2026-03-28 19:37:07 -0500

    Add after_sign_in_path_for redirect method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,3 +25,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:address, :city, :postal_code, :province_id])
   end
 end
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      admin_dashboard_path
+    else
+      root_path
+    end
+  end
